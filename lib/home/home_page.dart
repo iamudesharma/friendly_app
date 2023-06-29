92e34491a0dcfcb57e5998c384eed126ee12e03d
iamudesharma <udesh2568@gmail.com>
2023-06-29 17:52:26 +0530

    check location added

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:friendly_app/helpers/geolocator.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late LocationPermission checkPermissionValue;
+
+  bool _isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() async {
+    var _checkPermission = await Geolocator.checkPermission();
+
+    await checkPermissionData(_checkPermission);
+    setState(() {
+      _isLoading = false;
+      checkPermissionValue = _checkPermission;
+    });
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+          child: _isLoading
+              ? CircularProgressIndicator.adaptive()
+              : checkPermission(checkPermissionValue)),
+    );
   }
 }
